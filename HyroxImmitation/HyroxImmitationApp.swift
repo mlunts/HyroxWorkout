807ea8f5b554647b4241b8a17be71385189b76e7
Marina Lunts <marinalunts@gmail.com>
2026-04-29 13:33:30 +0200

    Initial Commit

---
+++ HyroxImmitation/HyroxImmitationApp.swift
@@ -0,0 +1,17 @@
+//
+//  HyroxImmitationApp.swift
+//  HyroxImmitation
+//
+//  Created by Marina Lunts on 29.04.26.
+//
+
+import SwiftUI
+
+@main
+struct HyroxImmitationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
